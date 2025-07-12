d9065e95553592ebc7bbef507e50cb3eb129d970
sasakidesu2024 <sasakidesu2024@gmail.com>
2025-07-12 19:51:20 +0900

    Initial commit

---
+++ js/code.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// $(function () {
+//     // ボタンがクリックされたら発動
+//     $(".hamburger").on("click", function () {
+//         // ドロワーメニュー部分のアニメーション
+//         $(".slide-menu").toggleClass("is-active");
+//         // ボタン部分のアニメーション
+//         $(".hamburger").toggleClass("is-active");
+//     });
+// });
+
+// 同じ内容
+
+
+
+$(function () {
+    $('.hamburger').click(function () {
+        $('.hamburger, .slide-menu').toggleClass('active');
+    });
+});//scroll_effect
+
+
+// WORKSの絵がスクロールしたら順番に出てくる 2行目から
+
+
+jQuery(function ($) {
+    $(window).on('load scroll', function () {
+
+        var box = $('.fadeup');
+        var animated = 'active';
+
+        box.each(function () {
+
+            var boxOffset = $(this).offset().top;
+            var scrollPos = $(window).scrollTop();
+            var wh = $(window).height();
+
+            if (scrollPos > boxOffset - wh + 100) {
+                $(this).addClass(animated);
+            }
+        });
+    });
+})
+
+// topにいくボタン
+
+$(function () {
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
